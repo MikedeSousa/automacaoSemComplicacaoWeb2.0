672425211c536b30e106964aaf1bd31e482960a0
Mike de Sousa <mikecostanascimento@gmail.com>
2022-04-13 17:44:51 -0300

    aula 08 design patterns

--- src/test/java/br/com/chronosAcademy/automacao/Web/TesteWeb.java
+++ src/test/java/br/com/chronosAcademy/automacao/Web/TesteWeb.java
@@ -1,28 +1,29 @@
 package br.com.chronosAcademy.automacao.Web;
 
+import br.com.chronosAcademy.core.Driver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import static org.junit.Assert.assertEquals;
 
 public class TesteWeb {
 
-    ChromeDriver driver;
+    WebDriver driver;
+    Driver driverWeb;
 
     @Before
     public void inicializaTeste(){
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
+        driverWeb = new Driver("chrome");
+        driver = driverWeb.getDriver();
         driver.get("https://www.chronosacademy.com.br");
     }
     @Test
     public void primeiroteste(){
-
         String xpathTitulo = "//section[2]//h4";
         WebElement txtTitulo = driver.findElement(By.xpath(xpathTitulo));
         String titulo = txtTitulo.getText();
